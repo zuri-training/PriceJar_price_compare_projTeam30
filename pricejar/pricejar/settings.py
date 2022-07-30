a77413da15831ca0202c3a0771c11b442fd770c9
Retdyel <77490836+Retdyel@users.noreply.github.com>
2022-07-30 17:41:31 +0100

    Added SMTP for password reset #6

--- pricejar/pricejar/settings.py
+++ pricejar/pricejar/settings.py
@@ -129,3 +129,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+# Simple mail transfer protocol for password reset
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '<Your Gmail address>'
+EMAIL_HOST_PASSWORD = '<Your Gmail password>'
+EMAIL_USE_TLS = True
