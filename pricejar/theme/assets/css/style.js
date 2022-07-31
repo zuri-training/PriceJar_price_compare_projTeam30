2a8979929661f9ee7ba2c20140326691bb73375e
vickode_X <75067007+victor21021996@users.noreply.github.com>
2022-07-31 19:29:47 +0100

    update

---
+++ pricejar/theme/assets/css/style.js
@@ -0,0 +1,13 @@
+function showList(e) {
+  var $gridCont = $('.grid-container');
+  e.preventDefault();
+  $gridCont.hasClass('list-view') ? $gridCont.removeClass('list-view') : $gridCont.addClass('list-view');
+}
+function gridList(e) {
+  var $gridCont = $('.grid-container')
+  e.preventDefault();
+  $gridCont.removeClass('list-view');
+}
+
+$(document).on('click', '.btn-grid', gridList);
+$(document).on('click', '.btn-list', showList);
