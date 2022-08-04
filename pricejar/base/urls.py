4b81d31e7c10477b8804a668f8e436e0336b7078
mirera <mirera.moses@gmail.com>
2022-08-04 15:06:58 +0300

    resolving conflict ii  #20

---
+++ pricejar/base/urls.py
@@ -0,0 +1,28 @@
+from unicodedata import name
+from django.urls import path,include
+from django.contrib.auth.views import LoginView, LogoutView 
+from django.contrib.auth.views import PasswordResetView,PasswordChangeView
+from . import views
+
+urlpatterns = [
+
+    path('', views.homePage, name='home'),
+
+    #user registration, login, logout url
+    path('register', views.signup_page, name='register'),
+    path('login', LoginView.as_view(
+           template_name='base/login.html',
+           redirect_authenticated_user=True),
+        name='login'),
+    path('logout', LogoutView.as_view(),name='logout'),
+
+    #password management url
+        # password reset
+    path('password_reset/', views.PasswordResetView.as_view(), name ='password_reset'), 
+    path('reset_password_sent/', views.PriceJarPassWordResetDoneView.as_view(), name ='password_reset_done'),
+        # password change
+    path('password_change/', views.PasswordChangeView.as_view(), name="account_change_password"),
+    path('accounts/password_change_done/',views.PriceJar_Password_Change_Done_View.as_view(),name="password_change_done"),
+
+    ] 
+
---
+++ pricejar/base/views.py
@@ -0,0 +1,58 @@
+
+from os import terminal_size
+from django.shortcuts import render
+from django.contrib.auth.views import PasswordResetView,PasswordChangeDoneView,PasswordChangeView,PasswordResetDoneView
+from django. contrib.auth.forms import PasswordResetForm,PasswordChangeForm
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib.auth import login
+from . import forms
+
+
+
+#homepage view
+def homePage(request):
+    return render(request, 'base/home.html')
+#end of homepage view
+
+
+#user registration functionality
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # auto-login user
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'base/register.html', context={'form': form})
+#end of user registration functionality
+
+ #PasswordChangeView
+class Password_Change_View(PasswordChangeView):
+    Form_class = PasswordChangeForm
+    success_url = reverse_lazy('PasswordChangeView')
+    template_name = 'registration/password_change_form.html'
+    title = gettext_lazy('password change')
+    #end of PasswordChangeView
+ 
+
+#PasswordResetView
+class Password_Reset_View(PasswordResetView):
+    template_name = "password_reset.html"
+#end of PasswordResetView
+
+
+#password_change_done_view
+class PriceJar_Password_Change_Done_View(PasswordChangeDoneView):
+    template_name = "password_change_done.html"
+    title = "Password Change Done Successfully"
+#end of password_change_done_view
+
+#password__reset_done_view
+class PriceJarPassWordResetDoneView(PasswordResetDoneView):
+    template_name = "password_reset_sent.html"
