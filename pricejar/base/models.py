a2df6d398b03d6a40cfaddd01c575c1ed6593e06
Retdyel <77490836+Retdyel@users.noreply.github.com>
2022-08-04 20:14:02 +0100

    User profile page render #14

--- pricejar/base/models.py
+++ pricejar/base/models.py
@@ -2,6 +2,17 @@ from pyexpat import model
 from tkinter import CASCADE
 from unicodedata import name
 from django.db import models
+from django.contrib.auth.models import User
+
 
 
 # Create your models here.
+
+# User profile
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
