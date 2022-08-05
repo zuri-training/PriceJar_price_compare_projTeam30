5fdc8fd2855c12c860e7abfb66ebd78320fae810
mirera <mirera.moses@gmail.com>
2022-08-05 17:35:14 +0300

    added saving df flash mesages #23

--- pricejar/base/views.py
+++ pricejar/base/views.py
@@ -5,13 +5,15 @@ from django.contrib.auth.views import PasswordResetView,PasswordChangeDoneView,P
 from django. contrib.auth.forms import PasswordResetForm,PasswordChangeForm
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.auth import login
 from django.views import View
 from . import forms
-from django.contrib.auth.decorators import login_required
+
 
 
 
@@ -24,13 +26,18 @@ def homePage(request):
 #user registration functionality
 def signup_page(request):
     form = forms.SignupForm()
+
     if request.method == 'POST':
         form = forms.SignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
             # auto-login user
             login(request, user)
             return redirect(settings.LOGIN_REDIRECT_URL)
+        else:
+            messages.error(request, 'An error occurred during regestration, please try again')
     return render(request, 'base/register.html', context={'form': form})
 #end of user registration functionality
 
