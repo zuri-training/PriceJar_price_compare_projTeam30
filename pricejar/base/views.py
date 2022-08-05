bc356a4033da181fdcc24131d3d20f1ca21265a0
Retdyel <77490836+Retdyel@users.noreply.github.com>
2022-08-06 00:23:04 +0100

    Added contact to views #21

--- pricejar/base/views.py
+++ pricejar/base/views.py
@@ -10,7 +10,7 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.contrib.auth import login
 from . import forms
-
+from .models import Contact
 
 
 #homepage view
@@ -44,3 +44,18 @@ class PriceJar_Password_Change_Done_View(PasswordChangeDoneView):
     template_name = "password_change_done.html"
     title = "Password Change Done Successfully"
 #end of password_change_done_view
+
+# Contact
+def contact(request):
+    if request.method == "POST":
+        contact = Contact()
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        contact.name = name
+        contact.email = email
+        contact.message = message
+        contact.save()
+    return render(request, 'base/contact.html')
+# End
