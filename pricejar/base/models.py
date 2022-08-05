3459a53a933cc948391bab1c3ec90ff6d207f55e
Retdyel <77490836+Retdyel@users.noreply.github.com>
2022-08-06 00:21:19 +0100

    Created contact model #21

--- pricejar/base/models.py
+++ pricejar/base/models.py
@@ -1,3 +1,4 @@
+import email
 from pyexpat import model
 from tkinter import CASCADE
 from unicodedata import name
@@ -5,3 +6,10 @@ from django.db import models
 
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
