9afdda51673ab75fb48f5734c513f33e7bb771c5
Retdyel <77490836+Retdyel@users.noreply.github.com>
2022-08-06 15:13:01 +0100

    Worked on the issues raised #21

--- pricejar/base/admin.py
+++ pricejar/base/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import UserProfile
+from .models import Contact
 # Register your models here.
 
 
 # Register UserProfile
 admin.site.register(UserProfile)
+
+# Register Contact
+admin.site.register(Contact)
