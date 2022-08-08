65faf0811e25b9bf93dc37d46ec6888218a0f7d9
Emmanuel Peter <Peterojonilemi@gmail.com>
2022-08-08 12:26:36 +0100

    deleted some codes

--- pricejar/base/views.py
+++ pricejar/base/views.py
@@ -101,32 +101,9 @@ def Userprofile(request):
 # end of user profile
 
 # Contact
-# def contact(request):
-    
-#     if request.method == "POST":
-#         # form = ContactForm(request.POST)
-#         firstname = request.POST["firstname"]
-#         lastname = request.POST["lastname"]
-#         email = request.POST["email"]
-#         message = request.POST["message"]
-
-#         Contact.objects.create(firstname = firstname,lastname = lastname, email = email , message = message).save()
-#         return redirect('/#contact')
-        # if form.is_valid():
-        #     form.save()
-        #     messages.add_message(request, messages.INFO, 'Your contact information and message were successfully submitted.')
-
-    # form = ContactForm()
-    # context = {'form': form} 
-    # else:
-    #     return render(request, 'base/contact.html')
-# End
-# def contact_view(request):
-#     context = {}
-#     context['form'] = ContactForm
-#     return render(request, '../templates/Contact_us.html', context)
-
-
 def contact(request):
-    
-    return render(request, 'base/contact.html')
+    context = {}
+    context['form'] = ContactForm
+    return render(request, 'base/contact.html', context)
+
+
