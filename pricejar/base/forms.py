ac2d4e3e9a6a9afb0492579393967b20346642ba
Emmanuel Peter <Peterojonilemi@gmail.com>
2022-08-08 09:20:56 +0100

    Imported forms  from django to render widgets

--- pricejar/base/forms.py
+++ pricejar/base/forms.py
@@ -1,7 +1,7 @@
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-
+from django import forms
 from django.forms import ModelForm
 from .models import Contact
 
@@ -13,7 +13,12 @@ class SignupForm(UserCreationForm):
 
 
 # model form for contact model
-class ContactForm(ModelForm):
+class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
+        widgets = {'firstname' :  forms.TextInput(attrs = {'type':'name','id':'loginName','placeholder':'Mawuli'}),
+        'lastname' :   forms.TextInput(attrs = {'type':'name','id':'loginName','placeholder':'Owusu'}),
+        'email'  : forms.TextInput(attrs = {'type':'email','id':'registerEmail','placeholder':'MawuliOwusu@gmail.com'}),
+        'message' :  forms.Textarea(attrs = {'class':'form-control rounded-0','id':'exampleFormControlTextarea1','rows':'10','placeholder':'What would you like to tell us?'}),}
+        
