5f628bb644fbbec4eff75ecef8139ec4beeb34d3
NwaObed <Obed.Nwachukwu@warwick.ac.uk>
2022-08-09 22:10:18 +0100

    staticfiles added

--- pricejar/settings.py
+++ pricejar/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+import django_on_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -149,3 +152,6 @@ LOGIN_REDIRECT_URL = "/"
 # Media URL and ROOT for user profile images
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+django_on_heroku.settings(locals())
