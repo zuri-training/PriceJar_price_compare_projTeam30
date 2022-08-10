3edea1be400751c5aae8f94d9e2dd4b55081c0b7
NwaObed <Obed.Nwachukwu@warwick.ac.uk>
2022-08-10 22:07:15 +0100

    more bugs removed

--- base/apps.py
+++ base/apps.py
@@ -4,10 +4,7 @@ from django.apps import AppConfig
 class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> own
+
 
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -16,8 +13,4 @@ class UserConfig(AppConfig):
     # corrected import error
     def ready(self):
         import base.signals
-<<<<<<< HEAD
-=======
->>>>>>> 113443751e7132b0b294df84db685278a2e26bb9
-=======
->>>>>>> own
+
