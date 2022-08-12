1acd91f39d6df15f2636fe10fc74989205e16c5f
enigma <enigma@Wilsons-MacBook-Pro.local>
2022-08-12 17:02:30 +0300

    registered scraper models#153

--- scraper/admin.py
+++ scraper/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import JumiaMobilePhoneModel
+from .models import KillaMallMobilePhone
+from .models import JumiaFasionModel
+from .models import KillaMallFasionModel
 
 # Register your models here.
+
+admin.site.register(JumiaMobilePhoneModel)
+admin.site.register(KillaMallMobilePhone)
+admin.site.register(JumiaFasionModel)
+admin.site.register(KillaMallFasionModel)
