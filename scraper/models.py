f40b5e99b0e7aa4616c17da64d283b2f9888d9e5
enigma <enigma@Wilsons-MacBook-Pro.local>
2022-08-13 13:25:31 +0300

    added shopit model and migrated #166

--- scraper/models.py
+++ scraper/models.py
@@ -21,6 +21,15 @@ class KillaMallMobilePhone(models.Model):
     def __str__(self):
         return self.product_name
 
+class ShopitMobilePhoneModel(models.Model):
+    image= models.URLField(_("Image"))
+    product_name=models.CharField(_("Product_Name"), max_length=500)
+    price=models.IntegerField(_("Price"))
+    Link=models.CharField(_("Link"),max_length=300)
+    
+    def __str__(self):
+        return self.product_name
+
 
 class JumiaFasionModel(models.Model):
     image= models.URLField(_("Image"))
