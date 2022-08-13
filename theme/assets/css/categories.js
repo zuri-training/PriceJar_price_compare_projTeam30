2e230eb559453eb7261c14cede8c209489157d78
vickode_X <75067007+victor21021996@users.noreply.github.com>
2022-08-13 14:07:36 +0100

    images updated

---
+++ theme/assets/css/categories.js
@@ -0,0 +1,11 @@
+$('#multi22').mdbRange({
+  single: {
+    active: true,
+    multi: {
+      active: true,
+      rangeLength: 2,
+      counting: true,
+      countingTarget: ['#ex3', '#ex4']
+    }
+  }
+});
