3ed9f301fbdcdb39c309ba0edc19fdee94558d40
Emmanuel Peter <Peterojonilemi@gmail.com>
2022-08-15 09:20:06 +0100

    rendered the newly included files, however, the responsiveness isn't 100%

---
+++ static/assets/css/categories.js
@@ -0,0 +1,13 @@
+$('#multi22').mdbRange({
+    single: {
+      active: true,
+      multi: {
+        active: true,
+        rangeLength: 2,
+        counting: true,
+        countingTarget: ['#ex3', '#ex4']
+      }
+    }
+  });
+  
+  
